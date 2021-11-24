93923c66ed6cb4cadb288c06f062cfc4d74f38f1
Maged Eltorkoman <newmaged@gmail.com>
2021-11-24 11:57:13 +0200

    dev: add converting between polar and cartesian functions

--- hsbalance/model.py
+++ hsbalance/model.py
@@ -1,5 +1,6 @@
 import cvxpy as cp
 import numpy as np
+import cmath as cm
 
 class Model:
 
@@ -22,5 +23,24 @@ class Model:
         return W.value
 
 
+
+def convert_to_polar(cart):
+    '''
+    docs: Convert complex number in the cartesian form
+          into polar form.
+    :inputs:
+    cart: Complex number in cartesian number ex. 12+23j
+         -> <class 'complex'>
+    output: Complex number in polar form (modulus, phase in degrees)
+            ex.(12, 90) -> <class 'tuple'>
+    '''
+    phase = cm.phase(cart) * 180 / cm.pi
+    if phase<0:
+            phase= phase+360
+    return (abs(cart), phase)
+
+def convert_to_cartesian(polar):
+    theta = polar[1] * cm.pi / 180
+    return polar[0]*cm.cos(theta)+ polar[0]* cm.sin(theta)*1j
 model = Model()
 W = model.solve()
--- hsbalance/tests/simple_least_square.py
+++ hsbalance/tests/simple_least_square.py
@@ -13,5 +13,21 @@ expected = np.array([[-1.04-1.66j],
                     [-.55+.91j]])
 my_model = model.Model(name='simple_least_square', A=A, ALPHA=ALPHA)
 W = my_model.solve()
-def test():
+def test_simple_LSE():
     assert max((W- expected)/W) < 0.01
+
+def test_convert_to_polar():
+    output = model.convert_to_polar(6+8j)
+    expected = (10,53.13)
+    error = max((x-y)/y for x,y  in zip(output, expected))
+    print('output=', output, '\n expected', expected
+         , '\nerror', error)
+    assert  error < 0.01
+
+def test_convert_to_cartesian():
+    output = model.convert_to_cartesian((4, 30))
+    expected = 3.4641+2j
+    error = abs(output - expected)
+    print('output=', output, '\n expected', expected
+         , '\nerror', error)
+    assert  error < 0.01
